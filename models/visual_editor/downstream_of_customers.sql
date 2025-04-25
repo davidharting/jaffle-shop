b53e756734e551a44e7ab50cc776dfa0c456b86f
David Harting <hartingdavid@icloud.com>
2025-04-25 11:33:20 -0400

    made changes to downstream_of_customers

--- models/visual_editor/downstream_of_customers.sql
+++ models/visual_editor/downstream_of_customers.sql
@@ -1,8 +1,14 @@
 WITH customers AS (
   /* Customer overview data mart, offering key details for each unique customer. One row per customer. */
   SELECT
-    *
+    customer_id,
+    customer_name,
+    count_lifetime_orders,
+    first_ordered_at,
+    lifetime_spend,
+    customer_type
   FROM {{ ref('jaffle_shop', 'customers') }}
+  LIMIT 25
 ), downstream_of_customers_sql AS (
   SELECT
     *
