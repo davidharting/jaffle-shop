e2a65f1c49d751dbb02eeecd30df871c6c270a00
David Harting <hartingdavid@icloud.com>
2025-04-25 11:15:08 -0400

    second commit to the workspace

--- models/canvas/downstream_of_customers.sql
+++ models/canvas/downstream_of_customers.sql
@@ -1,7 +1,8 @@
 WITH customers AS (
   /* Customer overview data mart, offering key details for each unique customer. One row per customer. */
   SELECT
-    *
+    customer_id,
+    customer_name
   FROM {{ ref('jaffle_shop', 'customers') }}
 ), downstream_of_customers_sql AS (
   SELECT
